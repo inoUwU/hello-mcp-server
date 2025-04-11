63f20165565d3c7fc6aac4e47b1b353af92359dc
ino <87593133+inoUwU@users.noreply.github.com>
2025-04-11 23:49:08 +0900

    機能追加: Python実行ツールの実行ロジックを改善し、Dockerを使用してモジュールをインストールする機能を追加

--- python-exec/pyexec.go
+++ python-exec/pyexec.go
@@ -3,13 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/mark3labs/mcp-go/mcp"
 	"os"
 	"os/exec"
 	"path"
 	"strings"
-
-	"github.com/mark3labs/mcp-go/mcp"
-	"github.com/mark3labs/mcp-go/server"
 )
 
 // handlePythonExecution handles the execute-python tool codes
@@ -36,4 +34,41 @@ func handlePythonExecution(ctx context.Context, request mcp.CallToolRequest) (*m
 	if err != nil {
 		return mcp.NewToolResultError(fmt.Sprintf("Failed to write script to file")), nil
 	}
+
+	cmdArgs := []string{
+		"run",
+		"--rm",
+		"-v",
+		fmt.Sprintf("%s:/app", tmpDir),
+		"mcr.microsoft.com/playwright/python:1.49.1-noble",
+	}
+
+	shArgs := []string{}
+
+	// modules is given by llm
+	if len(modules) > 0 {
+		shArgs = append(shArgs, "python", "-m", "pip", "install", "--quiet")
+		shArgs = append(shArgs, modules...)
+		shArgs = append(shArgs, "&&")
+	}
+
+	shArgs = append(shArgs, "python", path.Join("app", "script.py"))
+	cmdArgs = append(cmdArgs, "sh", "-c", strings.Join(shArgs, " "))
+
+	cmd := exec.Command("docker", cmdArgs...)
+	out, err := cmd.Output()
+	if err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			return mcp.NewToolResultError(
+				fmt.Sprintf("Python exited with code %d: %s",
+					exitError.ExitCode(),
+					string(exitError.Stderr),
+				),
+			), nil
+		}
+		return mcp.NewToolResultError(
+			fmt.Sprintf("Execution failed: %v")), nil
+	}
+	return mcp.NewToolResultText(string(out)), nil
+
 }
